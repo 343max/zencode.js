f289414dfe75784e866042b36aed490d66c6b8a2
Max Winde <max.winde@gmail.com>
2009-11-24 14:57:11 +0100

    Removed "subExpand" Method.

--- zencode.js
+++ zencode.js
@@ -59,52 +59,6 @@ jQuery.fn.expand = function(selector) {
 }
 
 jQuery.zen = {
-	subExpand: function(selector) {
-		var result = [];
-		
-		selector.replace(/^([^>+]+)(([>+])(.*))?/, function($0, instruction, $1, operator, subSelector) {
-			instruction.replace(/^([^*]+)(\*([0-9]+))?/, function($0, tag, multiplier, factor) {
-				if(factor == undefined) factor = 1;
-				if(factor < 1) factor = 1;
-				
-				//try { console.log(factor); } catch(e) {};
-				
-				var tagName = tag.match(/[^.#]+/)[0];
-				
-				for(var i = 1; i <= factor; i++) {
-					//try { console.dir(instruction); } catch(e) {};
-					
-					var el = $('<' + tagName + '>');
-					
-					tag.replace(/([.#])([^.#]+)/g, function($0, kind, name) {
-						if(kind == '#') {
-							el.attr('id', name);
-						} else if(kind == '.') {
-							el.addClass(name);
-						}
-					});
-					
-					result.push(el);
-
-					if(subSelector != undefined) {
-						var sub = jQuery.zen.subExpand(subSelector);
-						if(operator == '>') {
-							$.each(sub, function() {
-								el.append(this);
-							});
-						} else if(operator == '+') {
-							$.each(sub, function() {
-								result.push(this);
-							});
-						}
-					}
-				}
-			});
-		});
-		
-		return result;
-	},
-	
 	expand: function(selector) {
 		/*var r = $('<span>');
 		
